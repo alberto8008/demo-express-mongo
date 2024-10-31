0d019facd8b609267208ae99e5f94a8944912042
alberto8008 <alberto.mendez8008@gmail.com>
2024-10-31 14:34:17 -0400

    add validation for shopID

--- backend/controllers/settingsController.js
+++ backend/controllers/settingsController.js
@@ -1,10 +1,19 @@
 import { getSettingsById, setSettingsById } from "../services/settings.js";
 
 export const getSettings = async (req, res) => {
-  const result = await getSettingsById(req.query.id);
+  const id = Number(req.query.id);
+  if (!req.query.id || isNaN(id)) {
+    return res.status(400).send({ message: "Invalid ID: ID must be a number" });
+  }
+
+  const result = await getSettingsById(id);
   console.log("33333333", result);
-  if (!result) res.status(404).send({ message: "Not found" });
-  else res.send(result);
+
+  if (!result) {
+    res.status(404).send({ message: "Not found" });
+  } else {
+    res.send(result);
+  }
 };
 
 export const setSettings = async (req, res) => {
